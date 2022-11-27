44748ae41fb373b4b1c142168c70b6dcd2004dab
Mohammad-Abdullah <abdullahofficial2022@gmail.com>
2022-11-27 23:25:05 +0600

    mondodb connected and put url created for saving all users

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
+require('dotenv').config()
+const jwt = require('jsonwebtoken');
 
 app.use(cors())
 app.use(express.json())
@@ -10,6 +12,73 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lg5fruz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+function verifyJwt(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "unauthorized" })
+    }
+    const token = authHeader.split(" ")[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: "forbidden" })
+        }
+        req.decoded = decoded;
+        next()
+    });
+}
+
+
+
+// const { MongoClient } = require("mongodb");
+// Replace the uri string with your MongoDB deployment's connection string.
+// const uri =
+//   "mongodb+srv://<user>:<password>@<cluster-url>?writeConcern=majority";
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+async function run() {
+    try {
+        await client.connect();
+        const usersCollection = client.db("carmania").collection("users");
+        //to save a user
+
+        app.put('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const role = req.body.role;
+            const name = req.body.name;
+            const filter = { user: email };
+            const options = { upsert: true };
+            console.log(name);
+            const updateDoc = {
+                $set: {
+                    user: email,
+                    role: role,
+                    name: name
+                }
+            }
+            const updateUser = await usersCollection.updateOne(filter, updateDoc, options)
+            var token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN);
+            res.send({ accessToken: token })
+            console.log(email);
+        })
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
+
